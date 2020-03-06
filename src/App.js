8e895141e1d76f708a17966d0df56f60e3f4ae16
€mily <youallwhylime@gmail.com>
2020-03-06 16:55:52 -0500

    initial poems list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+    sortAlphabetical(list){
+        return list[1]
+    }
+
+    render(){
+        const poems = ['Silver', 'i am total lost, teehee', 'Let\'s Kill This Love', 'Downtown Train', 'Me and Summer: It\'s Complicated', 'Macy\'s Herald Square', 'Like You', 'Homemade Curiosity', 'Green Eyes', 'Galaxy', 'False Smiles', 'mm', 'May 6th', 'etc....']
+
+        const poemsList = poems.map((poem) =>
+            <li>{poem}</li>
+        );
+
+        return(
+            <div>
+                <div className="header" align="center">
+                    <h1>emily.writes.poems.</h1>
+                </div>
+                <div className="section-poems">
+                    <h2>my poems.</h2>
+                    <ul>{poemsList}</ul>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
