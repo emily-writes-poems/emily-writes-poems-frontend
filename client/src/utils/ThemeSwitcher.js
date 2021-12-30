616ae81b749b94a558f7f29e11840d327f88813a
emilywritescode <22375770+emilywritescode@users.noreply.github.com>
2021-12-30 14:13:44 -0500

    dynamic favicon

--- client/src/utils/ThemeSwitcher.js
+++ client/src/utils/ThemeSwitcher.js
@@ -5,7 +5,16 @@ const ThemeSwitcher = () => {
     const [mode, set_mode] = useLocalStorage('mode', 'day');
 
     useEffect(() => {
+        // set page styling
         document.body.className = mode;
+
+        //set favicon
+        let favicon = document.getElementById('favicon');
+        if(localStorage.getItem('mode') === 'day') {
+            favicon.setAttribute('href', './favicon.ico');
+        } else {
+            favicon.setAttribute('href', './favicon_dark.ico');
+        }
     }, [mode]);
 
     function toggle() {
