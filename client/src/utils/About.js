47c5cbf21b43e109e82884c069d62972cd76b8af
emilywritescode <22375770+emilywritescode@users.noreply.github.com>
2022-01-15 14:42:55 -0500

    updates for 2022

--- client/src/utils/About.js
+++ client/src/utils/About.js
@@ -19,16 +19,15 @@ const About = () => {
             </div>
 
             <div className='section'>
-                <h6 className='color-accent-1'>my "writing style".<span className={'about2 expand_hide ' + (about2Hidden ? 'expand' : 'hide') } onClick={() => setAbout2Toggle(!about2Hidden)}></span></h6>
+                <h6 className='color-accent-1'>my writing style.<span className={'about2 expand_hide ' + (about2Hidden ? 'expand' : 'hide') } onClick={() => setAbout2Toggle(!about2Hidden)}></span></h6>
                 <div className={'summary color-accent-2 ' + (about2Hidden ? 'hidden' : 'shown' )}>
-                    <p>Whatever comes out of my mind goes onto the page, and then I edit and refine wording and structure. I <i>love</i> using <a className='link-style' href="https://literarydevices.net/enjambment/">enjambment</a>; it's interesting, it's ambiguous, I love it. Most of my poems are in free verse, not following any specific meter or rhyming pattern. People have described my poems as "calming", "full of imagery", "slice of life", "having really unique details and descriptions" . . . thank you for all of those, it's lovely to hear! From my own reading, I've discovered some aspects of my "writing style":</p>
+                    <p>Whatever comes out of my mind goes onto the page, and then I edit and refine wording and structure. I <i>love</i> using <a className='link-style' href="https://literarydevices.net/enjambment/">enjambment</a>; it's interesting, it's ambiguous, I love it. Most of my poems are in free verse, not following any specific meter or rhyming pattern. I also tend to write "slice of life". From my own reading, I've discovered some aspects of my writing style:</p>
                     <ul>
-                        <li key='ws1'><i>salt. the taste of salt. salt in the air.</i> I write about it a surprising amount.</li>
-                        <li key='ws2'><i>vibrant, rich colors.</i> No pastel colors here. Red shows up often.</li>
-                        <li key='ws3'><i>connection.</i> Big theme in my writing.</li>
-                        <li key='ws4'><i>light & darkness.</i> For every poem that's set in sunshine, there's one that takes place in fog or night or shade.</li>
-                        <li key='ws5'><i>the moon.</i> This ties in with the light & darkness thing. The image of the moon always inspires my writing.</li>
-                        <li key='ws6'><i>comma splices.</i> It's like I don't know how to use commas. The rules of the English language are meant to be broken. I kinda like comma splices, don't you?</li>
+                        <li><i>salt. the taste of salt. salt in the air.</i> I write about it a surprising amount.</li>
+                        <li><i>vibrant, rich colors.</i> No pastel colors here. Red shows up often.</li>
+                        <li><i>light & darkness.</i> For every poem that's set in sunshine, there's one that takes place in fog or night or shade.</li>
+                        <li><i>the moon.</i> This ties in with the light & darkness thing. The image of the moon always inspires my writing.</li>
+                        <li><i>comma splices.</i> It's like I don't know how to use commas. The rules of the English language are meant to be broken. I kinda like comma splices, don't you?</li>
                     </ul>
                 </div>
             </div>
