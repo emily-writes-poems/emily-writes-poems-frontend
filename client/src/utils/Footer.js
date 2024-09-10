9001e5c073b2c79ba9aaa308ecadbc34df5cd7b0
emilywritescode <22375770+emilywritescode@users.noreply.github.com>
2024-09-09 21:22:51 -0400

    hey it's 2024 and i wrote some stuff i guess

--- client/src/utils/Footer.js
+++ client/src/utils/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <div className='container my-4 pb-2' align='center'>
             <hr />
-            <h6 className="font-3 color-accent-2 pt-2">Website by Emily Lau, 2023. Poem content and all text by Emily Lau, 2018-2023.</h6>
+            <h6 className="font-3 color-accent-2 pt-2">Website by Emily Lau, 2024. Poem content and all text by Emily Lau, 2018-2024.</h6>
             <p className="small font-3 color-accent-2">Follow the development of this webapp on <a className="link-style" href="https://www.github.com/emily-writes-poems">Github</a>.</p>
             <p className="small font-3"><Link className='link-style' to='/terms_privacy'>Terms & Privacy</Link></p>
         </div>
